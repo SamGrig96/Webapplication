e891ce9717e676559cc1a4208cdbe1637fb1464c
SamGrig96 <grigoryan.samik@gmail.com>
2020-12-14 14:14:03 +0400

    first commit

---
+++ build/precache-manifest.78e54a25f1a22324830b9af8ad776e3f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6013b45ae88d687cf842e4fd56e48e8f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "edecf859499e3f3017dc",
+    "url": "/static/css/main.e158c074.chunk.css"
+  },
+  {
+    "revision": "1378d54315d6e675661e",
+    "url": "/static/js/2.4324269c.chunk.js"
+  },
+  {
+    "revision": "9fcef6ad0bbd8023605b82fcc8a87493",
+    "url": "/static/js/2.4324269c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edecf859499e3f3017dc",
+    "url": "/static/js/main.0c5b7919.chunk.js"
+  },
+  {
+    "revision": "a45f0e8c82465c44312e",
+    "url": "/static/js/runtime-main.4a6dc895.js"
+  },
+  {
+    "revision": "9c67771a4ea3d3641fb6acf2ab3e1e9b",
+    "url": "/static/media/client.9c67771a.png"
+  },
+  {
+    "revision": "eda8b9935892766ddf1ef056e487f193",
+    "url": "/static/media/play.eda8b993.svg"
+  },
+  {
+    "revision": "d736e2e151beb04d1c715326b1a46ecf",
+    "url": "/static/media/waiting.d736e2e1.png"
+  }
+]);
